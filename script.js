1f9ed83deef7dfc2d60b0227751bc1563cc87c71
Vladislav Kraemer <v21kraemer@gmail.com>
2025-05-20 20:35:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const jokeBtn = document.getElementById("joke-btn");
 const generateJoke = () => {
   const xhr = new XMLHttpRequest();
 
-  xhr.open("GET", "https://api.chucknorris.io/jokes/random");
+ 
 
 
         
